ad8cb154c87c5c66a74dd4588dd537f9bed2d3e8
mehmet <mehmetakif3881@gmail.com>
2020-07-16 17:54:42 +0300

    it was done some mistakes for 1 number.

--- check_prime_num.py
+++ check_prime_num.py
@@ -1,7 +1,7 @@
 number = int(input("enter a integer to chech whether it's prime number or not: "))
 
 
-if 2 ** number % number == 2 or number == 1 or number == 2:
+if 2 ** number % number == 2 or number == 2:
     print(number ,"is a prime number")
 else:
     print(number, "is not a prime number")
